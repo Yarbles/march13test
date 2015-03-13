c67903402803f5942abff4be073a7670900113f1
Reid Baldwin <student@epicodus.com>
2015-03-13 16:27:35 -0700

    pass fourth test, no addt'l coding necessary

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,10 +4,9 @@
         function countRepeats($needle, $haystack)
         {
             $haystack_array = explode(" ", $haystack);
-
             $number_of_matches = 0;
-
             $haystack_matches = array_count_values($haystack_array);
+            
 
                 if (in_array($needle, $haystack_array)) {
                     $number_of_matches = $haystack_matches[$needle];
